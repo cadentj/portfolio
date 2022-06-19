0e804be443f5301b7d93adf4411b415a83c9a0ec
Caden <caden.juang@gmail.com>
2022-06-19 13:09:51 -0500

    Functional useHover Method

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -7,26 +7,53 @@ import Swipe from './Swiper';
 import Stack from '@mui/material/Stack';
 import { Container, Typography } from '@mui/material';
 import SwiperElement from './SwiperElement';
+import useHover from './useHover';
+
+
+const Slide = React.forwardRef((props,ref) => (
+    <Box ref={ref}>
+        <SwiperElement/>
+    </Box>
+));
+
 
 export default function ProjectContainer() {
+
+
+    const [hoverRef1, isHovered1] = useHover();
+    const [hoverRef2, isHovered2] = useHover();
+    const [hoverRef3, isHovered3] = useHover();
+    const [hoverRef4, isHovered4] = useHover();
+
+    const text = "featued";
+    const hoveredSlide = () => {
+        if (isHovered1) text = "ISP";
+        if (isHovered2) text =  "College Email Bot";
+        if (isHovered3) text =  "Mav App";
+        if (isHovered4) text =  "Community Tutoring";
+        text = "Featured Works";
+    }
+
     return (
         <Stack
             alignItems="center"
-            justifyContent="center"
             style={{ minHeight: '90vh' }}
         >
             <Stack
                 direction="row"
                 spacing="10px"
             >
-                <SwiperElement/>
-                <SwiperElement/>
-                <SwiperElement/>
-                <SwiperElement/>
+                <Slide ref={hoverRef1}/>
+                <Slide ref={hoverRef2}/>
+                <Slide ref={hoverRef3}/>
+                <Slide ref={hoverRef4}/>
             </Stack>
+
+
+
             <Container >
-                <Typography variant="h4" sx={{ textAlign: "center" }}>
-                    Featured Work
+                <Typography variant="h4" sx={{ textAlign: "center" }} border={1}>
+                    {text}
                 </Typography>
             </Container>
         </Stack>
--- src/components/SwiperElement.js
+++ src/components/SwiperElement.js
@@ -3,9 +3,11 @@ import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Box';
 
 export default function SwiperElement() {
+    
+
     return (
         <Container sx={{minHeight: "500px", minWidth: "100px", border: 1, background: "grey"}}>
-            
+           
         </Container>
     );
 }
