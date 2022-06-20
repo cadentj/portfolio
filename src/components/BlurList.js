9e83ea64d5a410d4785013bfdfada2a0cd044d8c
Caden <caden.juang@gmail.com>
2022-06-19 20:27:47 -0500

    List Work
    - Background Transitioning

--- src/components/BlurList.js
+++ src/components/BlurList.js
@@ -8,10 +8,24 @@ import ListItemText from '@mui/material/ListItemText';
 import '../styles/blur_list.css';
 import { Typography } from '@mui/material';
 
+const images = {
+    "one": "https://wallpaperaccess.com/full/217097.jpg",
+    "two": "https://www.teahub.io/photos/full/7-75812_league-of-legends-preseason-2020.png",
+    "three": "https://cdn.wallpapersafari.com/96/43/2iV0HB.jpg"
+}
 
 export default function BlurList() {
+
+    let defaultImg = "https://wallpaperaccess.com/full/217097.jpg";
+    const [text, setText] = React.useState(defaultImg);
+
     const Item = (props) => {
-        return <ListItem disablePadding>
+        return <ListItem
+            onMouseOver={() => {setText(images[props.image]);
+            console.log(text)}}
+            onMouseLeave={() => setText(defaultImg)}
+            disablePadding
+        >
             <ListItemText
                 primary={props.name}
                 primaryTypographyProps={{
@@ -19,22 +33,23 @@ export default function BlurList() {
                     fontWeight: 'medium',
                     variant: 'h2',
                 }}
-                className="li"
+                className="li two"
             />
         </ListItem>
     }
 
     return (
-        <Box className="back" height="200vh">
-
-            <Box sx={{ width: '100%' }} className="list-heading">
-                <Typography variant="h3" sx={{ textAlign: "center", fontWeight: "medium" }}>Work</Typography>
-                <Box sx={{ width: '100%' }} className="list-container">
-                    <Item name="Housing Precarity Model" />
-                    <Item name="Community Tutoring" />
-                    <Item name="Mav App 2.0" />
-                    <Item name="Maverick Debate" />
-                    <Item name="Razors 'N' Blade" />
+        <Box className="back" height="200vh" sx={{background:`url('${text}')`}}>
+            <Box className="overlay">
+                <Box sx={{ width: '100%' }} className="list-heading">
+                    <Typography variant="h3" sx={{ textAlign: "center", fontWeight: "medium" }}>Work</Typography>
+                    <Box sx={{ width: '100%' }} className="list-container">
+                        <Item name="Housing Precarity Model" image="one" />
+                        <Item name="Community Tutoring" image="two" />
+                        <Item name="Mav App 2.0" image="three" />
+                        {/* <Item name="Maverick Debate" image="four" />
+                        <Item name="Razors 'N' Blade" image="five" /> */}
+                    </Box>
                 </Box>
             </Box>
         </Box>
