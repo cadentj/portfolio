12a5baf333226d37b0fd3fdc3453c9bf2e7dc716
Caden <caden.juang@gmail.com>
2022-06-21 15:41:50 -0500

    Updated Animation
    - Updated animation from three-experiment-repo
    - Added acceleration(ish) movement, new star background
    - Rotating planet and stars to create effect of movement
    - Removed actual movement

--- src/components/ShipAnimation.js
+++ src/components/ShipAnimation.js
@@ -1,21 +1,22 @@
+import ReactDOM from 'react-dom'
 import { Box } from '@mui/system';
 import { useRef, Suspense } from 'react';
 import { Canvas, useThree, useLoader, useFrame } from "@react-three/fiber";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { CubeTextureLoader, Vector3, } from "three";
+import { CubeTextureLoader, Vector3 } from "three";
 
 function SkyBox() {
     const { scene } = useThree();
     const loader = new CubeTextureLoader();
     // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
     const texture = loader.load([
-        "./space.jpg",
-        "./space.jpg",
-        "./space.jpg",
-        "./space.jpg",
-        "./space.jpg",
-        "./space.jpg",
+        "./square.png",
+        "./square.png",
+        "./square.png",
+        "./square.png",
+        "./square.png",
+        "./square.png",
     ]);
 
     // Set the scene background property to the resulting texture.
@@ -24,36 +25,68 @@ function SkyBox() {
 }
 
 const Planets = () => {
-    const gltf = useLoader(GLTFLoader, "scene.gltf");
+    const gltf = useLoader(GLTFLoader, "./scene.gltf");
 
-    return <primitive object={gltf.scene} scale={8} />;
+    const ref = useRef();
+    useFrame((state) => (ref.current.rotation.y += 0.01));
+
+
+    const planetMesh = <mesh
+        ref={ref}
+        position={[10, 0, 10]}
+    >
+        <primitive object={gltf.scene} position={[-5, 0, 12.5]} scale={8} />
+    </mesh>;
+
+
+
+
+    return planetMesh;
 };
-const dummy = new Vector3()
 
+const Milky = () => {
+    const gltf = useLoader(GLTFLoader, "./need_some_space/scene.gltf");
+
+    const ref = useRef();
+    useFrame((state) => (ref.current.rotation.y += 0.005));
+
+    return <mesh
+        ref={ref}
+    >
+        <primitive object={gltf.scene} position={[-2140, -2140, 2130]} scale={1500} />
+    </mesh>;
+};
+
+const dummy = new Vector3()
 
 const Ship = () => {
 
     const ref = useRef()
 
 
-    const fbx = useLoader(FBXLoader, "X-Wing.fbx");
+    const fbx = useLoader(FBXLoader, "./X-Wing.fbx");
 
-    useFrame((state) => {
-        ref.current.position.z -= 0.013;
 
-        state.camera.position.lerp(dummy.set(0, 0, 0), 0.001)
-    })
+    //Old Camera following behind jet
+    // useFrame((state) => {
+        //ref.current.position.z -= 0.013;
+
+        //state.camera.position.lerp(dummy.set(0, 0, 0), 0.001)
+    // })
 
     return (
         <mesh
             ref={ref}
-            position={[0, 0, 15]}
+            position={[0, 2, 15]}
             rotation={[0, Math.PI, 0]}
         >
             <primitive object={fbx} scale={0.0005} />
         </mesh>
     );
 };
+
+let x = 0;
+let y = 0;
 
 const MouseTrackingShip = () => {
 
@@ -62,11 +95,12 @@ const MouseTrackingShip = () => {
 
     useFrame((state) => {
         const mouse = state.mouse;
-        const x = (mouse.x * viewport.width) / 100;
-        const y = (mouse.y * viewport.height) / 100;
+        const xChange = (mouse.x * viewport.width) / 10000;
+        const yChange = (mouse.y * viewport.width) / 10000;
+        x += (x < 0.25 && x > -0.25) ? xChange : ((x >= 0) ? -0.0002 : 0.0002);
+        y += (y < 0.10 && y > -0.20) ? yChange : ((y >= 0) ? -0.0002 : 0.0002);
         // Besides testing, how am I supposed to know which positional argument is position vs point?
-        ref.current.position.set(x, y, 0)
-        ref.current.rotation.set(y, -x, 0)
+        ref.current.rotation.set(-y, x, 0)
     })
 
     return (
@@ -80,13 +114,13 @@ const MouseTrackingShip = () => {
 export default function Animation() {
     return (
         <Box height="100vh">
-            <Canvas camera={{ fov: 70, position: [0, 0, 16] }}>
+            <Canvas camera={{ fov: 70, position: [0, 2, 18] }}>
 
                 <directionalLight position={[10, 10, 5]} intensity={2} />
                 <directionalLight position={[-10, -10, -5]} intensity={1} />
 
                 <Suspense fallback={null}>
-
+                    <Milky/>
                     <Planets />
                     <SkyBox />
 
